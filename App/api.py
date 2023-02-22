d094e7599316b1d740bd5b8fffb27a16aec07e6d
Dylan Rutter <dylan.rutter@my.wheaton.edu>
2023-02-22 12:09:51 -0600

    Update api.py
    Updated so salary is no longer a necessary input

--- App/api.py
+++ App/api.py
@@ -1,63 +1,73 @@
-import json
-from typing import Any
-
-import numpy as np
-import pandas as pd
-from fastapi import APIRouter, HTTPException
+from fastapi import FastAPI, HTTPException
+import joblib, numpy
+from pydantic import BaseModel, Field 
 from fastapi.encoders import jsonable_encoder
-from car_evaluation_model import __version__ as model_version
-from car_evaluation_model.predict import make_prediction
-from loguru import logger
+import pandas as pd
+from data import process_data
 
-from app import __version__, schemas
-from app.config import settings
-
-"""
-The health endpoint is quite straightforward. 
-It returns the health response schema of the model when you access the web server (Figure 4).
- You defined this schema in the health.py module in the schemas directory. 
-
-The predict endpoint is slightly more complex. Here are the steps involved: 
-
-Take the input and convert it into a pandas DataFrame: the jsonable_encoder returns a JSON compatible version of the pydantic model.   
-Log the input data for audit purposes. 
-Make a prediction using the ML model’s make_prediction function. 
-Catch any errors made by the model. 
-Return the results, if the model has no errors.
-
-"""
-api_router = APIRouter()
-
-
-@api_router.get("/health", response_model=schemas.Health, status_code=200)
-def health() -> dict:
+class Item(BaseModel):
     """
-    Root Get
+    Define inputs and types
+    Modify column names with hyphens for compatibility
     """
-    health = schemas.Health(
-        name=settings.PROJECT_NAME, api_version=__version__, model_version=model_version
-    )
+    age: int
+    workclass: object
+    fnlgt: int
+    education: object
+    education_num: int = Field(alias="education-num") 
+    marital_status: object = Field(alias="marital-status")
+    occupation: object
+    relationship: object
+    race: object
+    sex: object
+    capital_gain: int = Field(alias="capital-gain")
+    capital_loss: int = Field(alias="capital-loss")
+    hours_per_week: int = Field(alias="hours-per-week")
+    native_country: object = Field(alias="native-country")
 
-    return health.dict()
+    class Config:
+        allow_population_by_field_name = True
 
+# Load models, they were stored in the prior directory
+cv_rfc = joblib.load('../models/rfc_model.pkl')
+lrc = joblib.load('../models/logistic_model.pkl')
 
-@api_router.post("/predict", response_model=schemas.PredictionResults, status_code=200)
-async def predict(input_data: schemas.MultipleCarTransactionInputData) -> Any:
+# Initialize an instance of FastAPI
+app = FastAPI()
+
+# Define the default route, return json of message
+@app.get("/")
+async def root():
+    return {"Message": "This is a Salary Prediction Model API"}
+
+# Define the route to the sample predictor
+@app.post("/predict_salary") 
+async def predict_salary(sample: Item): 
     """
-    Make predictions with the Fraud detection model
+    Input a instance of raw data
+    Return value is a salary prediction
     """
+    # Confirm input sample is valid
+    if(not(sample)): 
+        raise HTTPException(status_code=400, 
+                            detail="Please Provide a valid sample")
+    # jsonable_encoder converts BaseModel object to json
+    answer_dict = jsonable_encoder(sample)
+    salary = "" 
 
-    input_df = pd.DataFrame(jsonable_encoder(input_data.inputs))
+    for key, value in answer_dict.items():
+        answer_dict[key] = [value]
+    person = pd.DataFrame.from_dict(answer_dict) # Make df   
+    person = process_data(person) # Process data for model compatability
+    prediction = cv_rfc.predict(person) # Predict on created df
 
-    # Advanced: You can improve performance of your API by rewriting the
-    # `make prediction` function to be async and using await here.
-    logger.info(f"Making prediction on inputs: {input_data.inputs}")
-    results = make_prediction(inputs=input_df.replace({np.nan: None}))
+    # Determine person's salary prediction
+    if(prediction[0] == 0):
+        salary = ">50k" 
 
-    if results["errors"] is not None:
-        logger.warning(f"Prediction validation error: {results.get('errors')}")
-        raise HTTPException(status_code=400, detail=json.loads(results["errors"]))
-
-    logger.info(f"Prediction results: {results.get('predictions')}")
-
-    return results
+    elif(prediction[0] == 1):
+        salary = "<=50k" 
+        
+    return {
+            "salary": salary 
+           }
